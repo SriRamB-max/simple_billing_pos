4c08b489c7eea780e5a7cda635fd615a775ff056
SriramB <rsri87560@gmail.com>
2025-07-24 14:10:05 +0530

    change in base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/simple_billing_pos"
+  base: "/simple_billing_pos/"
 })
